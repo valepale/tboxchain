17e075f7dfcba3b5fdd04804837e30411d270f27
valeria <valeetpale@gmail.com>
2018-04-13 18:15:05 +0200

    ripristinato nav.js

--- src/Components/Navigation/Nav.js
+++ src/Components/Navigation/Nav.js
@@ -30,14 +30,9 @@ class Nav extends React.Component {
   unregisterDropdown(dropdownComponent) {
     this.registeredDropdowns = this.registeredDropdowns.filter(i => i !== dropdownComponent);
   }
-  
-  getHomepage() {
-     return Scrivito.Obj.getByPath("/");
-  }
 
   render() {
     return (
-      <Scrivito.LinkTag to={ this.getHomepage() } > HOME </>
       <Scrivito.ChildListTag
         className="nav navbar-nav navbar-right"
         parent={ Scrivito.Obj.root() }
