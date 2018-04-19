6588521dcdd334432c8edcb6e89f9909b9d54b4b
valeria <valeetpale@gmail.com>
2018-04-19 17:12:52 +0200

    correzione fullNavigation.js

--- src/Components/Navigation/FullNavigation.js
+++ src/Components/Navigation/FullNavigation.js
@@ -4,66 +4,68 @@ import CollapseToggle from './CollapseToggle';
 import Logo from './Logo';
 import Nav from './Nav';
 import { SearchBox, SearchIcon } from './Search';
+import LanguageSwitch from './LanguageSwitch';
 
 class FullNavigation extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      expanded: false,
-    };
+        this.state = {
+            expanded: false,
+        };
 
-    this.toggleExpanded = this.toggleExpanded.bind(this);
-    this.closeExpanded = this.closeExpanded.bind(this);
-  }
+        this.toggleExpanded = this.toggleExpanded.bind(this);
+        this.closeExpanded = this.closeExpanded.bind(this);
+    }
 
-  toggleExpanded() {
-    this.setState({ expanded: !this.state.expanded });
-  }
+    toggleExpanded() {
+        this.setState({expanded: !this.state.expanded});
+    }
 
-  closeExpanded() {
-    this.setState({ expanded: false });
-  }
+    closeExpanded() {
+        this.setState({expanded: false});
+    }
 
-  render() {
-    const {
-      bootstrapNavbarClassNames,
-      toggleSearch,
-      scrolled,
-      navigationStyle,
-      showSearch,
-    } = this.props;
+    render() {
+        const {
+            bootstrapNavbarClassNames,
+            toggleSearch,
+            scrolled,
+            navigationStyle,
+            showSearch,
+        } = this.props;
 
-    const classNames = [
-      'navbar',
-      'navbar-fixed-top',
-      'navbar-default',
-      ...bootstrapNavbarClassNames,
-    ];
+        const classNames = [
+            'navbar',
+            'navbar-fixed-top',
+            'navbar-default',
+            ...bootstrapNavbarClassNames,
+        ];
 
-    return (
-      <nav className={ classNames.join(' ') } >
-        <div className="container">
-          <SearchBox toggleSearch={ toggleSearch } showSearch={ showSearch } />
-
-          <div className="navbar-header">
-            <CollapseToggle
-              expanded={ this.state.expanded }
-              toggleExpanded={ this.toggleExpanded }
-            />
-            <Logo scrolled={ scrolled } navigationStyle={ navigationStyle } />
-            <SearchIcon toggleSearch={ toggleSearch } />
-          </div>
-
-          <BootstrapCollapse in={ this.state.expanded }>
-            <div className="navbar-collapse">
-              <Nav closeExpanded={ this.closeExpanded } expanded={ this.state.expanded } />
-            </div>
-          </BootstrapCollapse>
-        </div>
-      </nav>
-    );
-  }
+        return (
+                <nav className={ classNames.join(' ') } >
+                    <div className="container">
+                        <SearchBox toggleSearch={ toggleSearch } showSearch={ showSearch } />
+                
+                        <div className="navbar-header">
+                            <CollapseToggle
+                                expanded={ this.state.expanded }
+                                toggleExpanded={ this.toggleExpanded }
+                                />
+                            <Logo scrolled={ scrolled } navigationStyle={ navigationStyle } />
+                            <LanguageSwitch />
+                            <SearchIcon toggleSearch={ toggleSearch } />
+                        </div>
+                
+                        <BootstrapCollapse in={ this.state.expanded }>
+                            <div className="navbar-collapse">
+                                <Nav closeExpanded={ this.closeExpanded } expanded={ this.state.expanded } />
+                            </div>
+                        </BootstrapCollapse>
+                    </div>
+                </nav>
+                );
+    }
 }
 
 
