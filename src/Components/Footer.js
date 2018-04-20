96ab0af51121b93514bf71c976c517a9a2de61d7
vpalermo <vpalermo@tabsrl.com>
2018-04-20 12:14:31 +0200

    correzione bug Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import * as Scrivito from 'scrivito';
+import getHomepage from '../../utils/getHomepage.js';
 
 function Footer() {
-  const root = Scrivito.Obj.root();
+  const root = getHomepage();
 
   if (!root) { return null; }
 
