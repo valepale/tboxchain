40658ab120cb3231d735f6558eaf5628090ca9a0
valeria <valeetpale@gmail.com>
2018-05-02 20:28:51 +0200

    bug funcitons

--- functions/test.js
+++ functions/test.js
@@ -1,21 +1,12 @@
-const fetch = require('node-fetch');
-
 exports.handler = function(event, context, callback) {
-  const respond = ({ status, body }) => {
-    callback(null, {
-      statusCode: status,
-      body: JSON.stringify({ body }),
-    });
-  };
-
-  (() => {
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-      .then(response => response.json())
-      .then(json => {
-        respond({ status: 200, body: json.body });
-      })
-      .catch(err => {
-        respond({ status: 422, body: "Couldn't get the data" });
-      });
-  })();
+  // Currently this only works with variables that are defined in the UI and not in the netlify.toml
+  // Using ES6 destructuring to pull the variable called FAKE_TOKEN out of the process.env object
+  // You can then use the FAKE_TOKEN anywhere in your code just as you would any string variable
+  const { FAKE_TOKEN } = process.env;
+  console.log(FAKE_TOKEN);
+  // Amazon callback function without any custom headers
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ token: FAKE_TOKEN }),
+  });
 };
