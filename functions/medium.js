20fb9a07a20ea4079ced488d3014d4aa35b8c91b
vpalermo <vpalermo@tabsrl.com>
2018-05-02 12:52:13 +0200

    percorso get alla functions

--- functions/medium.js
+++ functions/medium.js
@@ -1,8 +1,13 @@
-
-exports.medium = function(event, context, callback) {
-    console.log('medium');
-    callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-    });
-}
+const functions = require('firebase-functions');
+var request = require('request');
+exports.medium = functions.https.onRequest((req, res) => {
+  if(!req.query.username) {
+    return res.status(400).send('Error: You need to include query param ?username=@yourUsername');
+  }
+  const url = `https://medium.com/${req.query.username}/latest?format=json`;
+  return request(url,(error, response, body) => {
+    const prefix = `])}while(1);</x>`
+    const strip = payload => payload.replace(prefix, ``)
+    res.send(JSON.parse(strip(body)));
+  });
+})
