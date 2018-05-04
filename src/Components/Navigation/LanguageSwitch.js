94da955b70810e0de213837b1de1a9caffc46a8b
valeria <valeetpale@gmail.com>
2018-05-04 20:21:23 +0200

    migliorie per responsice

--- src/Components/Navigation/LanguageSwitch.js
+++ src/Components/Navigation/LanguageSwitch.js
@@ -5,14 +5,15 @@ import getHomepage from '../../utils/getHomepage';
 function LanguageSwitch() {
   const homepages = [...Scrivito.getClass('Homepage').all()];
   return (
-    <ul className="nav navbar-nav">
-      { homepages.map(homepage =>
-        <li key={homepage.id()}>
-          <Scrivito.LinkTag to={ homepage }>
+    <ul className="nav navbar-nav text-center">       
+        <li>
+         { homepages.map(homepage =>
+          <Scrivito.LinkTag to={ homepage } className="d-inline-block text-uppercase">
             { homepage.path().substr(6, 2) }
           </Scrivito.LinkTag>
+           ) }
         </li>
-      ) }
+          
     </ul>
   );
 }
