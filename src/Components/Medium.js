5702fac8e3604d9b7b3d75fc49ae7af2ba5b6e17
valeria <valeetpale@gmail.com>
2018-05-09 19:25:09 +0200

    sistemazioni widget medium post in ita ed in en

--- src/Components/Medium.js
+++ src/Components/Medium.js
@@ -77,11 +77,9 @@ class Medium extends React.Component {
                     const aStyle = {
                         textDecoration: 'none'
                     };
-                    if (i === 0) {
-                        return <div className="col-lg-12" key={i}><a key={i} style={aStyle} href={obj.link} target="_blank" title={title}><div><img key={i} src={obj.thumbnail} kay={i}/></div><div><h2 key={i}><strong>{obj.title}</strong></h2> </div><div><p>{contentExtract}...</p></div><p key={i} className="text-color-verde">{obj.author}<br /><i key={i} className="fa fa-calendar fa-1x" ></i><span style={spanStyle}>&nbsp;{data}</span></p></a></div>
-                    } else {
-                        return <div className="col-lg-6" key={i}><a key={i} style={aStyle} href={obj.link} target="_blank" title={title}><div><img key={i} src={obj.thumbnail} kay={i}/></div><div><h2 key={i}><strong>{obj.title}</strong></h2> </div><div><p>{contentExtract}...</p></div><p key={i} className="text-color-verde">{obj.author}<br /><i key={i} className="fa fa-calendar fa-1x" ></i><span style={spanStyle}>&nbsp;{data}</span></p></a></div>
-                    }
+
+                    return <div className="col-lg-6" key={i}><a key={i} style={aStyle} href={obj.link} target="_blank" title={title}><div><img key={i} src={obj.thumbnail} kay={i}/></div><div><h2 key={i}><strong>{obj.title}</strong></h2> </div><div><p>{contentExtract}...</p></div><p key={i} className="text-color-verde">{obj.author}<br /><i key={i} className="fa fa-calendar fa-1x" ></i><span style={spanStyle}>&nbsp;{data}</span></p></a></div>
+
                 })
                 )
 
