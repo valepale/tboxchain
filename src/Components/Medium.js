bd07421043ff8f016bfd8e01f775efde77451f9d
vpalermo <vpalermo@tabsrl.com>
2018-05-09 13:07:35 +0200

    correzioni widget medium post

--- src/Components/Medium.js
+++ src/Components/Medium.js
@@ -10,8 +10,7 @@ class Medium extends React.Component {
         this.state = {
             posts: []
         }
-
-
+        
     }
 
     componentDidMount() {
@@ -20,31 +19,68 @@ class Medium extends React.Component {
                 rss_url: 'https://medium.com/feed/tboxchain'
             }
         }).then(response => {
+           
             console.log('response.data', response);
-
-            this.setState({
-                posts: response.data.items
-            });
-
-        })
-                .catch(error => {
-                    console.log(error);
+            var contentsIta = [];
+            var contentsEn = [];
+            var contents = [];
+            console.log('this.state.posts', this.state.posts.length);
+            response.data.items.map(function (obj, i) {
+                var intCategories = obj.categories.length;
+                console.log('intCategories', intCategories);
+                obj.categories.map(function (category, j) {
+                    console.log('j', j);
+                    if (category === 'italiano') {
+                        console.log('ita', i);
+                        return contentsIta.push(obj);
+                    } 
+                    if (category !== 'italiano' && intCategories === j + 1){
+                        console.log('en', i);
+                        return contentsEn.push(obj);
+                    }
                 });
+
+
+
+            })
+               const currentPage = Scrivito.currentPage();
+        const path = currentPage.path();
+      
+
+        if (path.includes('/lang/en')) {
+            
+            contents = contentsEn;
+            console.log('contentsEn', contentsEn);
+        } else if (path.includes('/lang/it')) {
+            contents = contentsIta;
+            console.log('contentsIta', contentsIta);
+        }
+            this.setState({
+                posts: contents
+            });
+            
+        })
+                    .catch(error => {
+                        console.log(error);
+                    });
         ;
     }
     render() {
+        const currentPage = Scrivito.currentPage();
+        const path = currentPage.path();
+      var title='';
+
+        if (path.includes('/lang/en')) {
+      
+            moment.locale('en');
+             title = 'Go to the post';
+        } else if (path.includes('/lang/it')) {
+            moment.locale('it');
+             title = 'Vai al post';
+        }
+      
         return (
                 this.state.posts.map(function (obj, i) {
-                    const currentPage = Scrivito.currentPage();
-                    const path = currentPage.path();
-                    var title = '';
-                    if (path.includes('/lang/en')) {
-                        moment.locale('en');
-                        title = 'Go to the post';
-                    } else if (path.includes('/lang/it')) {
-                        moment.locale('en');
-                        title = 'Vai al post';
-                    }
                     const data = moment(obj.pubDate).format("l");
                     const contentAll = obj.content.replace(/<(?:.|\n)*?>/gm, "");
                     var contentExtract = contentAll.split(/\s+/).slice(0, 20).join(" ");
@@ -52,8 +88,8 @@ class Medium extends React.Component {
                         fontSize: '15px',
                         color: '#716f73'
                     };
-                    const aStyle ={
-                        textDecoration:'none'
+                    const aStyle = {
+                        textDecoration: 'none'
                     };
                     if (i === 0) {
                         return <div className="col-lg-12" key={i}><a key={i} style={aStyle} href={obj.link} target="_blank" title={title}><div><img key={i} src={obj.thumbnail} kay={i}/></div><div><h2 key={i}><strong>{obj.title}</strong></h2> </div><div><p>{contentExtract}...</p></div><p key={i} className="text-color-verde">{obj.author}<br /><i key={i} className="fa fa-calendar fa-1x" ></i><span style={spanStyle}>&nbsp;{data}</span></p></a></div>
