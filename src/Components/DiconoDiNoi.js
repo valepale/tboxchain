4b334df68cd2f5387e6c1a84162f4299c894f099
vpalermo <vpalermo@tabsrl.com>
2018-05-14 11:18:34 +0200

    aggiunti script in _headers

--- src/Components/DiconoDiNoi.js
+++ src/Components/DiconoDiNoi.js
@@ -5,7 +5,6 @@ import Slider from "react-slick";
 class DiconoDiNoi extends React.Component {
     constructor(props) {
         super(props);
-
     }
 
     render() {
@@ -45,7 +44,8 @@ class DiconoDiNoi extends React.Component {
             ]
         };
         const textNone = {
-            textDecoration: 'none'
+            textDecoration: 'none',
+            color: '#3d4142 !important'
         }
         const border = {
             border: "15px solid #fff",
@@ -57,73 +57,50 @@ class DiconoDiNoi extends React.Component {
                     <div style={border}>
                         <img src="https://cdn0.scrvt.com/55526e734dfec79f05b2ccc7bbe2c4f4/eb77d80f7df06376/2ca85e579793/v/72805777b91d/5.jpg" alt="" width="" heigth=""/>                     
                         <h3>                   
-                            <a href="http://veented.info/crexis/red-hair/" style={textNone}>Potenza, addio alle recensioni fasulle online</a>
+                            <a target="_blank" href="http://napoli.repubblica.it/cronaca/2018/05/11/news/potenza_addio_alle_recensioni_fasulle_online-196092323/?refresh_ce" style={textNone}>Potenza, addio alle recensioni fasulle online</a>
                         </h3>
                         <p className="post-details">
                             <i className="fa fa-clock-o"></i>
                             11 Maggio 2018					<i className="fa fa-user"></i>
-                            Posted By <span className="colored">ANNA MARTINO</span>
+                            <span className="colored"> Anna Martino</span>
                         </p>
                         <p className="post_message">
                         <p>Validare le recensioni turistiche on line attraverso il protocollo blockchain, il database in cui sono registrate tutte le transazioni fatte in Bitcoin, la moneta “virtuale” che permette di operare senza l’intermediazione delle banche. La metodologia individuata è quella della TboxChain...
                         </p>
                         </p>
-                        <a href="http://veented.info/crexis/red-hair/" className="post_read_more_button ex-link uppercase" style={textNone}>LEGGI</a>
+                        <a target="_blank" href="http://napoli.repubblica.it/cronaca/2018/05/11/news/potenza_addio_alle_recensioni_fasulle_online-196092323/?refresh_ce" className="post_read_more_button ex-link uppercase" style={textNone}>LEGGI</a>
                     </div>
                     <div style={border}>
                         <img src="https://cdn0.scrvt.com/55526e734dfec79f05b2ccc7bbe2c4f4/eb77d80f7df06376/2ca85e579793/v/72805777b91d/5.jpg" alt="" width="" heigth=""/>                     
                         <h3>                   
-                            <a href="http://veented.info/crexis/red-hair/" style={textNone}>Post from Blog</a>
+                            <a target="_blank" href="http://www.factorympresa.invitalia.it/news/-/blogs/factorympresa-turismo-selezionate-le-20-migliori-startup-che-parteciperanno-all-accelerathon-di-napoli?_33_redirect=http%3A%2F%2Fwww.factorympresa.invitalia.it%2Fnews%3Fp_p_id%3D33%26p_p_lifecycle%3D0%26p_p_state%3Dnormal%26p_p_mode%3Dview%26p_p_col_id%3Dcolumn-1%26p_p_col_count%3D1%26_33_struts_action%3D%252Fblogs%252Fview" style={textNone}>Factorympresa Turismo: selezionate le 20 migliori startup che parteciperanno all’Accelerathon di Napoli</a>
                         </h3>
                         <p className="post-details">
                             <i className="fa fa-clock-o"></i>
-                            on 18 Mar, 2016					<i className="fa fa-user"></i>
-                            Posted By <span className="colored">Michael</span>
+                            10 Maggio 2018				<i className="fa fa-user"></i>
+                            <span className="colored">Factory Impresa</span>
                         </p>
                         <p className="post_message">
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...
+                        <p>Grande successo della call di FactorYmpresa Turismo, il programma promosso dal Ministero dei Beni Culturali e gestito da Invitalia che offre servizi di tutoraggio e contributi economici...
                         </p>
                         </p>
-                        <a href="http://veented.info/crexis/red-hair/" className="post_read_more_button ex-link uppercase" style={textNone}>LEGGI</a>
+                        <a target="_blank" href="http://www.factorympresa.invitalia.it/news/-/blogs/factorympresa-turismo-selezionate-le-20-migliori-startup-che-parteciperanno-all-accelerathon-di-napoli?_33_redirect=http%3A%2F%2Fwww.factorympresa.invitalia.it%2Fnews%3Fp_p_id%3D33%26p_p_lifecycle%3D0%26p_p_state%3Dnormal%26p_p_mode%3Dview%26p_p_col_id%3Dcolumn-1%26p_p_col_count%3D1%26_33_struts_action%3D%252Fblogs%252Fview" className="post_read_more_button ex-link uppercase" style={textNone}>LEGGI</a>
                     </div>
                     <div style={border}>
                         <img src="https://cdn0.scrvt.com/55526e734dfec79f05b2ccc7bbe2c4f4/eb77d80f7df06376/2ca85e579793/v/72805777b91d/5.jpg" alt="" width="" heigth=""/>                     
                         <h3>                   
-                            <a href="http://veented.info/crexis/red-hair/" style={textNone}>Post from Blog</a>
+                            <a  target="_blank" href="http://vincenzomoretti.nova100.ilsole24ore.com/2018/05/01/i-sogni-di-michele-storie-di-silicio-di-stringhe-di-codice-e-di-sonno-perduto/" style={textNone}>I sogni di Michele. Storie di silicio, di stringhe di codice e di sonno perduto </a>
                         </h3>
                         <p className="post-details">
                             <i className="fa fa-clock-o"></i>
-                            on 18 Mar, 2016					<i className="fa fa-user"></i>
-                            Posted By <span className="colored">Michael</span>
+                            1 Maggio 2018					<i className="fa fa-user"></i>
+                            <span className="colored"> Vincenzo Moretti</span>
                         </p>
                         <p className="post_message">
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...
+                        <p>Caro Diario, Michele Cignarale l’ho conosciuto un settembre di due anni fa, a Potenza, nel corso di una bella festa realizzata ogni anno dalla Cgil Basilicata. Lo arruolo abbastanza presto nel dipartimento sognatori...
                         </p>
                         </p>
-                        <a href="http://veented.info/crexis/red-hair/" className="post_read_more_button ex-link uppercase" style={textNone}>LEGGI</a>
-                    </div>
-                    <div style={border}>
-                        <img src="https://cdn0.scrvt.com/55526e734dfec79f05b2ccc7bbe2c4f4/eb77d80f7df06376/2ca85e579793/v/72805777b91d/5.jpg" alt="" width="" heigth=""/>                     
-                        <h3>                   
-                            <a href="http://veented.info/crexis/red-hair/" style={textNone}>Post from Blog</a>
-                        </h3>
-                        <p className="post-details">
-                            <i className="fa fa-clock-o"></i>
-                            on 18 Mar, 2016					<i className="fa fa-user"></i>
-                            Posted By <span className="colored">Michael</span>
-                        </p>
-                        <p className="post_message">
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...
-                        </p>
-                        </p>
-                        <a href="http://veented.info/crexis/red-hair/" className="post_read_more_button ex-link uppercase" style={textNone}>LEGGI</a>
-                    </div>
-                    <div style={border}>
-                       
-                    </div>
-                
-                    <div style={border}>
-                       
+                        <a target="_blank" href="http://vincenzomoretti.nova100.ilsole24ore.com/2018/05/01/i-sogni-di-michele-storie-di-silicio-di-stringhe-di-codice-e-di-sonno-perduto/" className="post_read_more_button ex-link uppercase" style={textNone}>LEGGI</a>
                     </div>
                 
                 </Slider>
