e62a9fddf091052b5e69bc0469a30440db948922
valeria <valeetpale@gmail.com>
2018-05-15 14:32:48 +0200

    sistemaizonoi

--- src/Components/LinkedinPage.js
+++ src/Components/LinkedinPage.js
@@ -22,6 +22,7 @@ class LinkedinPage extends React.Component {
         script2.type = "IN/CompanyProfile";
         script2.setAttribute('data-id', 'tboxchain');
         script2.setAttribute('data-format', 'inline');
+        script2.setAttribute('data-width', '350');
         this.instance.appendChild(script2);
     }
 
