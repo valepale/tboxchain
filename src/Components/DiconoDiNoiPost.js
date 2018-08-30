b800fd1bea6d8a68dc7a45d25ff48b9ceded50d6
vpalermo <vpalermo@tabsrl.com>
2018-08-30 09:01:07 +0200

    abilitate frecce per il widget diconodinoipost

--- src/Components/DiconoDiNoiPost.js
+++ src/Components/DiconoDiNoiPost.js
@@ -13,7 +13,7 @@ var settings = {
 dots: true,
         infinite: false,
         speed: 500,
-        arrows: false,
+        arrows: true,
         slidesToShow: 3,
         slidesToScroll: 3,
         initialSlide: 0,
