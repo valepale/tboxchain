5179610f83aee67e1cf0e6385e433f3a29f13e16
vpalermo <vpalermo@tabsrl.com>
2018-09-04 10:48:03 +0200

    sostiuiti link sezione 'eventi e competizioni'

--- src/Components/EventiECompetizioni.js
+++ src/Components/EventiECompetizioni.js
@@ -16,10 +16,10 @@ class Loghi extends React.Component {
                                 <a href="https://www.buytourismonline.com/" target="_blank"><img className="no-margin" src="https://cdn0.scrvt.com/55526e734dfec79f05b2ccc7bbe2c4f4/64d327e11c811088/aa260d774223/v/e152c47f68d4/loghi_bianchi4_mod.png" alt="" width="" heigth="" /></a>
                             </div>
                             <div className="col-lg-3 col-md-4 col-xs-6">
-                                <a href="https://www.creativebusinesscupitalia.it/" target="_blank">  <img className="no-margin" src="https://cdn0.scrvt.com/55526e734dfec79f05b2ccc7bbe2c4f4/222a02eb3499d1fa/065afd759a19/v/726586e721fa/loghi_bianchi6_mod.png" alt="" width="" heigth="" /></a>
+                                <a href="https://www.creativebusinesscupitalia.it/tboxchain-vince-cbc-italia-2018/" target="_blank">  <img className="no-margin" src="https://cdn0.scrvt.com/55526e734dfec79f05b2ccc7bbe2c4f4/222a02eb3499d1fa/065afd759a19/v/726586e721fa/loghi_bianchi6_mod.png" alt="" width="" heigth="" /></a>
                             </div>
                             <div className="col-lg-3 col-md-4 col-xs-6">
-                                <a href="http://www.factorympresa.invitalia.it/" target="_blank"> <img className="no-margin" src="https://cdn0.scrvt.com/55526e734dfec79f05b2ccc7bbe2c4f4/82b10f6e840a2ea7/ab2481669e34/v/e9480dae676a/loghi_bianchi7_mod.png" alt="" width="" heigth="" /></a>
+                                <a href="http://www.factorympresa.invitalia.it/news/-/blogs/150227?_33_redirect=http%3A%2F%2Fwww.factorympresa.invitalia.it%2Fnews%3Fp_p_id%3D33%26p_p_lifecycle%3D0%26p_p_state%3Dnormal%26p_p_mode%3Dview%26p_p_col_id%3Dcolumn-1%26p_p_col_count%3D1%26_33_struts_action%3D%252Fblogs%252Fview" target="_blank"> <img className="no-margin" src="https://cdn0.scrvt.com/55526e734dfec79f05b2ccc7bbe2c4f4/82b10f6e840a2ea7/ab2481669e34/v/e9480dae676a/loghi_bianchi7_mod.png" alt="" width="" heigth="" /></a>
                             </div>
                           
                         </div>
